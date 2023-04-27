c0d95c641ece70d6cb7f5265d38fa82444e4fb33
ManuelonGithub <manuelonemail@gmail.com>
2023-04-27 19:32:30 -0400

    cleaned up printf_df - it's now a proof-of-concept function

--- printf_df/printf_df.h
+++ printf_df/printf_df.h
@@ -1,9 +1,11 @@
-#ifndef D_PRINTF_H_
-#define D_PRINTF_H_
+
+
+#ifndef PRINTF_DF_H_
+#define PRINTF_DF_H_
 
 #include <stdarg.h>
 
-int vprintf_df(void (*putc)(char), const char *fmt, va_list args);
+int vprintf_df(const char *fmt, va_list args);
 
 /**
  * @brief 	deferred printf
@@ -13,20 +15,6 @@ int vprintf_df(void (*putc)(char), const char *fmt, va_list args);
  * @warning	This function is not thread-safe/reentrant 
  * 			due to utilizing a callback function as an output
  */
-__attribute__ ((format (printf, 2, 3))) void printf_df(void (*putc)(char), const char* fmt, ...);
+__attribute__ ((format (printf, 1, 2))) void printf_df(const char* fmt, ...);
 
-
-int vsprintf_df(char* buf, const char *fmt, va_list args);
-
-/**
- * @brief deferred sprintf
- * @param	dst byte buffer to insert processed data into - output is not a c-string!
- * @param	fmt printf format string
- * @param	... variable arguments to be processed according to the format
- * @return	length of the content in dst
- * @note	This function is fully thread-safe/reetrant.
- * @warning	dst will not be c-string compatible. 
- */
-__attribute__ ((format (printf, 2, 3))) int sprintf_df(char* dst, const char* fmt, ...);
-
-#endif // !D_PRINTF_H_
+#endif // !PRINTF_DF_H_
